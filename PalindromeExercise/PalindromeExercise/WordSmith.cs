a93b11986036c78339d5809ca2128bbba4c98dbe
Kenneth Baker <spyksaturn@gmail.com>
2023-02-27 12:25:38 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word1)
+        {
+            string word2 = "";
+            for (int i = word1.Length - 1; i >= 0; i--)
+            {
+                word2 += word1[i];
+            }
+            if (word1 == word2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
+using PalindromeExercise;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("monkey", false)]
+        [InlineData("malayalam", true)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            //Arange
+            var word3 = new WordSmith();
+            //Act
+            var actual = word3.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
